12be5bf10ac9f37964dab4c303674cbf82f70053
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-09-16 19:49:09 +0500

    userProfile schema model is added

---
+++ models/userProfile.models.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose"
+
+const userProfileSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      bio: {
+        type: String,
+        trim: true
+      },
+      profilePicture: {
+        type: String 
+      },
+      status: {
+        type: String,
+        enum: ['online', 'offline', 'away'],
+        default: 'offline'
+      },
+      lastLogin: {
+        type: Date
+      }
+},{timestamps: true})
+
+const UserProfile = mongoose.model("UserProfile", userProfileSchema) 
