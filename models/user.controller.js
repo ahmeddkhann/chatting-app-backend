3d537353056af8f402fbc3c02a74f8c1ed27e098
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-09-18 19:24:55 +0500

    logout controller added

--- models/user.controller.js
+++ models/user.controller.js
@@ -2,6 +2,24 @@ import { ApiResponse } from "./ApiResponse"
 import { ApiError } from "./ApiError"
 import {asyncHandler} from "./asyncHandler"
 import { User } from "./user.models";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
+const generateAccessAndRefreshToken = async (userId) => {
+
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError (500, "error while generating access and refresh tokens")
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password, username, address, phone } = req.body;
@@ -41,6 +59,84 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body;
+
+    if (!email && !username) {
+        throw new ApiError(400, "Email or username is required");
+    }
+    if (!password) {
+        throw new ApiError(400, "Password is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ email }, { username }]
+    });
+
+    if (!user) {
+        throw new ApiError(403, "User does not exist! Please provide a valid email or username");
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new ApiError(401, "Password is incorrect");
+    }
+
+    const generatingToken = await generateAccessAndRefreshToken(user._id);
+    if (!generatingToken) {
+        throw new ApiError(500, "There is an issue in logging in the user");
+    }
+
+    return res.status(200).json({
+        accessToken: generatingToken.accessToken,
+        refreshToken: generatingToken.refreshToken,
+        user: {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+        }
+    });
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body;
+    
+    if (!email && !username) {
+        throw new ApiError(403, "Either username or email is required");
+    }
+    if (!password) {
+        throw new ApiError(403, "Password is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    });
+    
+    if (!user) {
+        throw new ApiError(402, "User does not exist");
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+        throw new ApiError(404, "Password is incorrect");
+    }
+
+    await User.findByIdAndUpdate(user.id, {
+        $unset: {
+            accessToken: 1,
+        }
+    });
+
+    return res.status(200).json({
+        status: 201,
+        message: "User logged out successfully"
+    });
+})
+
+
+
 export {
-    registerUser
+    registerUser,
+    loginUser,
+    logoutUser
 }
